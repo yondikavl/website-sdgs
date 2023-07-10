71fe47782bfefcd51c5d3051302909732dd53c40
Fitra Ilyasa <fitrailyasa12@gmail.com>
2023-07-11 00:39:22 +0700

    update

--- resources/views/admin/pencapaian/edit.blade.php
+++ resources/views/admin/pencapaian/edit.blade.php
@@ -19,30 +19,6 @@
         @csrf
         @method('PUT')
       <div class="card-body">
-        <div class="d-flex col-12 flex-row">
-          <div class="form-group col-6">
-            <label for="indikator_id">{{ __('ID Indikator Sekarang') }}</label>
-            <select class="form-control col-form-label rounded-2" name="indikator_id" id="indikator_id" disabled>
-              <option value="{{$pencapaian->indikator_id}}">{{$pencapaian->indikator_id}}</option>
-            </select>  
-              @error('indikator_id')
-                  <span class="invalid-feedback" role="alert">
-                      <strong>{{ $message }}</strong>
-                  </span>
-              @enderror
-          </div>
-          <div class="form-group col-6">
-            <label for="subindikator_id">{{ __('ID Sub Sekarang') }}</label>
-            <select class="form-control col-form-label rounded-2" name="subindikator_id" id="subindikator_id" disabled>
-              <option value="{{$pencapaian->subindikator_id}}">{{$pencapaian->subindikator_id}}</option>
-            </select>  
-              @error('subindikator_id')
-                  <span class="invalid-feedback" role="alert">
-                      <strong>{{ $message }}</strong>
-                  </span>
-              @enderror
-          </div>
-          </div>
           <div class="d-flex col-12 flex-row">
           <div class="form-group col-4">
             <label for="tahun">{{ __('Tahun') }}</label>
@@ -59,7 +35,7 @@
                 <option selected value="{{$pencapaian->tipe}}">{{$pencapaian->tipe}}</option>
                 <option value="%">Persen (%)</option>
                 <option value="orang">Orang</option>
-            </select>  
+            </select>
               @error('tipe')
                   <span class="invalid-feedback" role="alert">
                       <strong>{{ $message }}</strong>
@@ -78,39 +54,11 @@
           </div>
           <div class="d-flex col-12 flex-row">
           <div class="form-group col-6">
-            <label for="indikator_id">{{ __('ID Indikator Baru') }}</label>
-            <select class="form-control col-form-label rounded-2" name="indikator_id" id="indikator_id" required>
-              @foreach ($indikators as $indikator)
-              <option value="{{$indikator->id}}">{{$indikator->id}}. {{$indikator->nama_sub}}</option>
-              @endforeach
-            </select>  
-              @error('indikator_id')
-                  <span class="invalid-feedback" role="alert">
-                      <strong>{{ $message }}</strong>
-                  </span>
-              @enderror
-          </div>
-          <div class="form-group col-6">
-            <label for="subindikator_id">{{ __('ID Sub Baru') }}</label>
-            <select class="form-control col-form-label rounded-2" name="subindikator_id" id="subindikator_id" required>
-              @foreach ($subindikators as $subindikator)
-              <option value="{{$subindikator->id}}">{{$subindikator->id}}. {{$subindikator->nama_sub}}</option>
-              @endforeach
-            </select>  
-              @error('subindikator_id')
-                  <span class="invalid-feedback" role="alert">
-                      <strong>{{ $message }}</strong>
-                  </span>
-              @enderror
-          </div>
-          </div>
-          <div class="d-flex col-12 flex-row">
-          <div class="form-group col-6">
               <button type="submit" class="btn btn-success">{{ __('Simpan') }}</</button>
           </div>
           </div>
         </div>
-      <!-- /.card-body -->      
+      <!-- /.card-body -->
     </form>
   </div>
 @endsection
