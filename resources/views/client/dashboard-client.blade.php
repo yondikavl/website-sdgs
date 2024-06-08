5ea472a1f940c00eb2c6ec49622f4f6732c4778d
yondikavl <yondikaviolanda@gmail.com>
2024-06-08 09:00:57 +0700

    feat: dashboard test

--- resources/views/client/dashboard-client.blade.php
+++ resources/views/client/dashboard-client.blade.php
@@ -4,19 +4,54 @@
 
 @section('style')
     <style>
+        .container {
+            width: 80%;
+            margin: 15px auto;
+        }
 
+        h2 {
+            text-align: center;
+        }
     </style>
 @endsection
 
 @section('content')
 
-    <h1>Ini Dashboard</h1>
-
+    <div class="col-12">
+        <div class="container mb-5">
+            <div class="row">
+                <div class="col-lg-12 mx-auto">
+                    <h1 class="text-center font-weight-bold mb-5">Dashboard SDGs</h1>
+                </div>
+            </div>
+        </div>
+        <div>
+            <canvas id="myChart"></canvas>
+        </div>
+        <script src="script.js"></script>
+    </div>
 
 @endsection
 
 @section('script')
 
-
+    <script>
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+                datasets: [{
+                    label: 'apples',
+                    data: [12, 19, 3, 17, 6, 3, 7],
+                    backgroundColor: "rgba(153,255,51,0.6)"
+                }, {
+                    label: 'oranges',
+                    data: [2, 29, 5, 5, 2, 3, 10],
+                    backgroundColor: "rgba(255,153,0,0.6)"
+                }]
+            }
+        });
+    </script>
 
 @endsection
