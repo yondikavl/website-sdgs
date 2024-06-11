ab69f054f21c25f904f38de30db8bd4cb3b77951
yondikavl <yondikaviolanda@gmail.com>
2024-06-11 15:29:28 +0700

    fix: svg map

--- resources/views/layouts/client/dashpage.blade.php
+++ resources/views/layouts/client/dashpage.blade.php
@@ -1,8 +1,8 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light py-2 px-3 border rounded-lg border-width-3">
+<nav class="navbar navbar-expand-lg navbar-light bg-light px-3 border rounded-lg border-width-3">
     <div class="collapse navbar-collapse" id="navbarNav">
         <div class="mx-auto">
             <ul class="navbar-nav d-flex justify-content-center">
-                <li class="nav-item mx-2 my-2 my-md-0">
+                <li class="nav-item mx-2 my-md-0">
                     <a class="nav-link text-sm bg-success rounded-lg px-3 py-2 text-center"
                         style="min-width: 180px; max-width: 180px" href="#"><i class="fa-solid fa-circle-info mx-2"
                             style="color: #ffffff;"></i>Overview</a>
