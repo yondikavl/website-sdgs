f28ab610d551e5a5046b53f8c96ccf94d5cea624
yondikavl <yondikaviolanda@gmail.com>
2024-06-12 13:29:59 +0700

    fix: map

--- resources/views/layouts/client/dashpage.blade.php
+++ resources/views/layouts/client/dashpage.blade.php
@@ -4,17 +4,17 @@
             <ul class="navbar-nav d-flex justify-content-center">
                 <li class="nav-item mx-2 my-md-0">
                     <a class="nav-link text-sm bg-success rounded-lg px-3 py-2 text-center"
-                        style="min-width: 180px; max-width: 180px" href="#"><i class="fa-solid fa-circle-info mx-2"
+                        style="min-width: 250px; max-width: 250px" href="#"><i class="fa-solid fa-circle-info mx-2"
                             style="color: #ffffff;"></i>Overview</a>
                 </li>
                 <li class="nav-item mx-2 my-2 my-md-0">
                     <a class="nav-link text-sm bg-success rounded-lg px-3 py-2 text-center"
-                        style="min-width: 180px; max-width: 180px" href="#"><i
+                        style="min-width: 250px; max-width: 250px" href="#"><i
                             class="fa-solid fa-chart-simple mx-2" style="color: #ffffff;"></i>Prediksi</a>
                 </li>
                 <li class="nav-item mx-2 my-2 my-md-0">
                     <a class="nav-link text-sm bg-success rounded-lg px-3 py-2 text-center"
-                        style="min-width: 180px; max-width: 180px" href="#"><i
+                        style="min-width: 250px; max-width: 250px" href="#"><i
                             class="fa-solid fa-magnifying-glass mx-2" style="color: #ffffff;"></i>Analisis
                         Pembanding</a>
                 </li>
