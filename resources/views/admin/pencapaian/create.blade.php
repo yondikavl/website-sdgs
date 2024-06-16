46b74bca19cc800fafdf2c147523c9e4e3c46a84
yondikavl <yondikaviolanda@gmail.com>
2024-06-16 21:38:11 +0700

    fix: btn close preview

--- resources/views/admin/pencapaian/create.blade.php
+++ resources/views/admin/pencapaian/create.blade.php
@@ -37,8 +37,8 @@
                         @csrf
                         <div class="form-group">
                             <label for="tujuan_id">{{ __('ID Tujuan') }}</label>
-                            <select class="form-control col-form-label rounded-2" name="tujuan_id" id="tujuan_id" onchange="getIndikator(this.value)"
-                                required>
+                            <select class="form-control col-form-label rounded-2" name="tujuan_id" id="tujuan_id"
+                                onchange="getIndikator(this.value)" required>
                                 <option value="">Pilih Tujuan</option>
                                 @foreach ($tujuans as $tujuan)
                                     <option value="{{ $tujuan->id }}">{{ $tujuan->id }}.
@@ -53,8 +53,8 @@
                         </div>
                         <div class="form-group">
                             <label for="indikator_id">{{ __('ID Indikator') }}</label>
-                            <select class="form-control col-form-label rounded-2" name="indikator_id"
-                                id="indikator_id" required>
+                            <select class="form-control col-form-label rounded-2" name="indikator_id" id="indikator_id"
+                                required>
                                 <option value="">Pilih Indikator</option>
                             </select>
                             @error('indikator_id')
@@ -101,10 +101,10 @@
 
                         <div class="form-group">
                             <label for="persentase">{{ __('Sumber Data') }}</label>
-                            <div class="input-group" >
+                            <div class="input-group">
                                 <input type="text" class="form-control @error('persentase') is-invalid @enderror"
-                                    placeholder="Masukkan sumber data" name="sumber_data"
-                                    required autocomplete="current-sumber_data">
+                                    placeholder="Masukkan sumber data" name="sumber_data" required
+                                    autocomplete="current-sumber_data">
                             </div>
                             @error('persentase')
                                 <span class="invalid-feedback" role="alert">
@@ -125,16 +125,21 @@
                         <!-- form start -->
                         <div class="card-body">
                             @if (auth()->user()->roles_id == 1)
-                                <form method="POST" action="{{ route('super.pencapaian.import') }}" enctype='multipart/form-data'>
-                            @elseif (auth()->user()->roles_id == 2)
-                                <form method="POST" action="{{ route('admin.pencapaian.import') }}" enctype='multipart/form-data'>
-                            @elseif (auth()->user()->roles_id == 3)
-                                <form method="POST" action="{{ route('opd.pencapaian.import') }}" enctype='multipart/form-data'>
+                                <form method="POST" action="{{ route('super.pencapaian.import') }}"
+                                    enctype='multipart/form-data'>
+                                @elseif (auth()->user()->roles_id == 2)
+                                    <form method="POST" action="{{ route('admin.pencapaian.import') }}"
+                                        enctype='multipart/form-data'>
+                                    @elseif (auth()->user()->roles_id == 3)
+                                        <form method="POST" action="{{ route('opd.pencapaian.import') }}"
+                                            enctype='multipart/form-data'>
                             @endif
                             @csrf
                             <div class="form-group">
                                 <label for="files">{{ __('File') }}</label>
-                                <input type="file" class="form-control @error('File') is-invalid @enderror" id="files" placeholder="Masukkan File" name="files[]" multiple="true" required autocomplete="file" autofocus onchange="previewFiles()">
+                                <input type="file" class="form-control @error('File') is-invalid @enderror"
+                                    id="files" placeholder="Masukkan File" name="files[]" multiple="true" required
+                                    autocomplete="file" autofocus onchange="previewFiles()">
                                 @error('File')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -143,13 +148,17 @@
                             </div>
                             <div class="form-group">
                                 <label for="tahun">{{ __('Tahun') }}</label>
-                                <input type="tahun" class="form-control @error('tahun') is-invalid @enderror" id="tahun" placeholder="2020" name="tahun" required autocomplete="tahun" autofocus>
+                                <input type="tahun" class="form-control @error('tahun') is-invalid @enderror"
+                                    id="tahun" placeholder="2020" name="tahun" required autocomplete="tahun"
+                                    autofocus>
                                 @error('tahun')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                                <a target="_blank" href="https://docs.google.com/spreadsheets/d/1WSfNsuYFjru5dCCjRN-Zn9c4hvdsWB5N/edit?usp=drive_link&ouid=106704558331702404617&rtpof=true&sd=true">Download template file</a>
+                                <a target="_blank"
+                                    href="https://docs.google.com/spreadsheets/d/1WSfNsuYFjru5dCCjRN-Zn9c4hvdsWB5N/edit?usp=drive_link&ouid=106704558331702404617&rtpof=true&sd=true">Download
+                                    template file</a>
                             </div>
                             <div class="mt-4">
                                 <button type="submit" class="btn btn-success">{{ __('Simpan') }}</button>
@@ -163,70 +172,86 @@
             </div>
         </div>
     </div>
-    <!-- Modal -->
-<div class="modal fade" id="filePreviewModal" tabindex="-1" aria-labelledby="filePreviewModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="filePreviewModalLabel">Preview Isi File</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <div id="file-content" class="table-responsive"></div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-danger" data-dismiss="modal">Tutup</button>
+
+
+    <div class="modal fade" id="filePreviewModal" tabindex="-1" aria-labelledby="filePreviewModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="filePreviewModalLabel">Preview Isi File</h5>
+                    <button id="btn_modal_close" type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div id="file-content" class="table-responsive"></div>
+                </div>
+                <div class="modal-footer">
+                    <button id="btn_modal_close" type="button" class="btn btn-danger"
+                        data-dismiss="modal">Tutup</button>
+                </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
 
 @section('script')
-<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.17.0/xlsx.full.min.js"></script>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.17.0/xlsx.full.min.js"></script>
 
-<script>
-    function getIndikator(id) {
-        $('#indikator_id').empty();
-        $('#indikator_id').append(`<option value="">Pilih Indikator</option>`);
-        $.ajax({
-            type: 'GET',
-            url: "{{ route('get-indikator', '') }}"+'/'+id,
-            success: function(response) {
-                response.forEach(element => {
-                    $('#indikator_id').append(`<option value="${element['kode_indikator']}">${element['kode_indikator']}. ${element['nama_indikator']}</option>`);
-                });
-            }
-        });
-    }
-
-    function previewFiles() {
-        const content = document.getElementById('file-content');
-        const files = document.getElementById('files').files;
-
-        content.innerHTML = ''; // Clear previous content
-
-        for (const file of files) {
-            const reader = new FileReader();
-            reader.onload = (e) => {
-                const data = new Uint8Array(e.target.result);
-                const workbook = XLSX.read(data, {type: 'array'});
-                const firstSheetName = workbook.SheetNames[0];
-                const worksheet = workbook.Sheets[firstSheetName];
-                const html = XLSX.utils.sheet_to_html(worksheet, {id: "data-table", editable: false});
-                content.innerHTML += html;
-            };
-            reader.readAsArrayBuffer(file);
+    <script>
+        function getIndikator(id) {
+            $('#indikator_id').empty();
+            $('#indikator_id').append(`<option value="">Pilih Indikator</option>`);
+            $.ajax({
+                type: 'GET',
+                url: "{{ route('get-indikator', '') }}" + '/' + id,
+                success: function(response) {
+                    response.forEach(element => {
+                        $('#indikator_id').append(
+                            `<option value="${element['kode_indikator']}">${element['kode_indikator']}. ${element['nama_indikator']}</option>`
+                        );
+                    });
+                }
+            });
         }
 
-        // Show the modal after processing the files
-        $('#filePreviewModal').modal('show');
-    }
-</script>
+        function previewFiles() {
+            const content = document.getElementById('file-content');
+            const files = document.getElementById('files').files;
+
+            content.innerHTML = '';
+
+            for (const file of files) {
+                const reader = new FileReader();
+                reader.onload = (e) => {
+                    const data = new Uint8Array(e.target.result);
+                    const workbook = XLSX.read(data, {
+                        type: 'array'
+                    });
+                    const firstSheetName = workbook.SheetNames[0];
+                    const worksheet = workbook.Sheets[firstSheetName];
+                    const html = XLSX.utils.sheet_to_html(worksheet, {
+                        id: "data-table",
+                        editable: false
+                    });
+                    content.innerHTML += html;
+                };
+                reader.readAsArrayBuffer(file);
+            }
+
+
+            $('#filePreviewModal').modal('show');
+        }
+
+        $(document).ready(function() {
+            $('#btn_modal_close').on('click', function() {
+                $('#filePreviewModal').modal('hide');
+            });
+        });
+    </script>
 @endsection
