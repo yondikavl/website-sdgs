da8e46c6c8216d83773d197e05cfedb58f80dea1
yondikavl <yondikaviolanda@gmail.com>
2024-07-04 10:41:57 +0700

    fix: geo responsive

--- resources/views/client/prediksi.blade.php
+++ resources/views/client/prediksi.blade.php
@@ -71,6 +71,7 @@
         </div>
 
         <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/simple-statistics"></script>
     </div>
 @endsection
 
@@ -82,42 +83,73 @@
             var ctx = document.getElementById('myChart').getContext('2d');
 
             // Data historis (2019-2024)
-            var historicalData = [60, 70, 80, 70, 80, 75];
+            var historicalData1 = [60, 70, 80, 70, 80, 75];
+            var historicalData2 = [65, 75, 85, 75, 85, 80];
+            var historicalData3 = [55, 65, 75, 65, 75, 70];
             var labels = ['2019', '2020', '2021', '2022', '2023', '2024'];
 
-            // Fungsi untuk memprediksi data 5 tahun ke depan
+            // Fungsi untuk memprediksi data 6 tahun ke depan menggunakan regresi linier
             function predictData(data, years) {
-                var growthRate = (data[data.length - 1] - data[0]) / data.length;
-                var predictedData = [...data];
-                for (var i = 1; i <= years; i++) {
-                    predictedData.push(predictedData[predictedData.length - 1] + growthRate);
+                var x = [];
+                var y = data;
+                for (var i = 0; i < y.length; i++) {
+                    x.push(i);
                 }
-                return predictedData.slice(data.length);
+                var regression = ss.linearRegression(x.map(function(d, i) {
+                    return [d, y[i]];
+                }));
+                var line = ss.linearRegressionLine(regression);
+
+                var predictedData = [];
+                for (var i = y.length; i < y.length + years; i++) {
+                    predictedData.push(line(i));
+                }
+                return predictedData;
             }
 
-            // Data prediksi 5 tahun ke depan (2025-2029)
-            var predictedData = predictData(historicalData, 6);
+            // Data prediksi 6 tahun ke depan (2025-2030)
+            var predictedData1 = predictData(historicalData1, 6);
+            var predictedData2 = predictData(historicalData2, 6);
+            var predictedData3 = predictData(historicalData3, 6);
             var forecastLabels = labels.concat(['2025', '2026', '2027', '2028', '2029', '2030']);
 
             // Menggabungkan data historis dan prediksi
-            var combinedData = historicalData.concat(predictedData);
+            var combinedData1 = historicalData1.concat(predictedData1);
+            var combinedData2 = historicalData2.concat(predictedData2);
+            var combinedData3 = historicalData3.concat(predictedData3);
 
             var myChart = new Chart(ctx, {
                 type: 'line',
                 data: {
                     labels: forecastLabels,
                     datasets: [{
-                        label: '1.1.1 Tingkat Kemiskinan',
-                        data: combinedData,
-                        borderColor: "rgba(75, 192, 192, 1)",
-                        backgroundColor: "rgba(75, 192, 192, 0.2)",
-                        fill: false
-                    }]
+                            label: '1.1.1 Tingkat Kemiskinan',
+                            data: combinedData1,
+                            borderColor: "rgba(75, 192, 192, 1)",
+                            backgroundColor: "rgba(75, 192, 192, 0.2)",
+                            fill: false
+                        },
+                        {
+                            label: '1.2.1 Tingkat Pengangguran',
+                            data: combinedData2,
+                            borderColor: "rgba(255, 99, 132, 1)",
+                            backgroundColor: "rgba(255, 99, 132, 0.2)",
+                            fill: false
+                        },
+                        {
+                            label: '1.3.1 Tingkat Pendidikan',
+                            data: combinedData3,
+                            borderColor: "rgba(54, 162, 235, 1)",
+                            backgroundColor: "rgba(54, 162, 235, 0.2)",
+                            fill: false
+                        }
+                    ]
                 },
                 options: {
                     scales: {
                         y: {
                             beginAtZero: true,
+                            min: 0,
                             max: 100
                         }
                     },
